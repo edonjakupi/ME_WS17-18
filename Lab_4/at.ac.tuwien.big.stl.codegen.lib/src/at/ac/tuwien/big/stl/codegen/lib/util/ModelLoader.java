09fae591b03f7fb9aff529bc197fd1507f24f32a
alexbamp <alex.bampoulidis@gmail.com>
2018-01-09 16:18:47 +0100

    B.1

--- Lab_4/at.ac.tuwien.big.stl.codegen.lib/src/at/ac/tuwien/big/stl/codegen/lib/util/ModelLoader.java
+++ Lab_4/at.ac.tuwien.big.stl.codegen.lib/src/at/ac/tuwien/big/stl/codegen/lib/util/ModelLoader.java
@@ -1,8 +1,17 @@
 package at.ac.tuwien.big.stl.codegen.lib.util;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.URIConverter;
+import java.io.File;
+import java.util.Map;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import at.ac.tuwien.big.stl.STLPackage;
+import at.ac.tuwien.big.stl.simulation.STLSimulationPackage;
 import at.ac.tuwien.big.stl.simulation.SimulationModel;
 
 public class ModelLoader {
@@ -17,7 +26,22 @@ public class ModelLoader {
 	 */
 	public static SimulationModel load(String path) {
 		// TODO
-		return null;
+		
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("stl", new XMIResourceFactoryImpl());
+		m.put("stlsimulation", new XMIResourceFactoryImpl());
+		
+		ResourceSet resourceSet = new ResourceSetImpl();
+		
+		resourceSet.getPackageRegistry().put(STLPackage.eINSTANCE.getNsURI(),STLPackage.eINSTANCE);
+		resourceSet.getPackageRegistry().put(STLSimulationPackage.eINSTANCE.getNsURI(),STLSimulationPackage.eINSTANCE);
+		
+		URI fileUri = createURI(path);
+		
+		Resource resource = resourceSet.getResource(fileUri, true);
+		
+		return (SimulationModel) resource.getContents().get(0);
 	}
 
 	/**
--- Lab_4/at.ac.tuwien.big.stl.codegen.lib/src/at/ac/tuwien/big/stl/codegen/lib/util/ModelStore.java
+++ Lab_4/at.ac.tuwien.big.stl.codegen.lib/src/at/ac/tuwien/big/stl/codegen/lib/util/ModelStore.java
@@ -1,7 +1,15 @@
 package at.ac.tuwien.big.stl.codegen.lib.util;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 import at.ac.tuwien.big.stl.simulation.SimulationModel;
 
@@ -18,6 +26,26 @@ public class ModelStore {
 	 */
 	public static void store(SimulationModel simModel, String path) {
 		// TODO
+		
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("stl", new XMIResourceFactoryImpl());
+		m.put("stlsimulation", new XMIResourceFactoryImpl());
+		
+		ResourceSet resourceSet = new ResourceSetImpl();
+		
+		URI fileUri = createURI(path);
+		
+		Resource resource = resourceSet.createResource(fileUri);
+		
+		resource.getContents().add(simModel);
+		
+		try {
+			resource.save(Collections.EMPTY_MAP);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
